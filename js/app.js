9e245795321c98bb41c63ddc84f916b1ff9e1342
Kris3579 <kristianesvelt@hotmail.com>
2018-07-11 19:50:31 -0700

    added tally of questions and alerts to the user

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 "use strict";
+var tally = 0;
+var username = prompt('Hello, and welcome to Kris\'s About-me page! What is your name?');
 var question1 = prompt('Is Kris a Dog person?');
 
 if (question1.toUpperCase() === 'N' || question1.toUpperCase() === 'NO') {
     alert('Correct!');
     document.getElementById("check1").style.display = "inline-block";
+    tally++;
+    console.log('number of tallies', tally);
 } else if (question1.toUpperCase() === 'Y' || question1.toUpperCase() === 'YES') {
     alert('Incorrect');
 } else {
@@ -16,6 +20,8 @@ var question2 = prompt('Was Kris born in Seattle?');
 if (question2.toUpperCase() === 'Y' || question1.toUpperCase() === 'YES') {
     alert('Correct!');
     document.getElementById("check2").style.display = "inline-block";
+    tally++;
+    console.log('number of tallies', tally);
 } else if (question2.toUpperCase() === 'N' || question1.toUpperCase() === 'NO') {
     alert('Incorrect');
 } else {
@@ -28,6 +34,8 @@ var question3 = prompt('Does Kris\'s ancestry include Native American blood?');
 if (question3.toUpperCase() === 'Y' || question1.toUpperCase() === 'YES') {
     alert('Correct!');
     document.getElementById("check3").style.display = "inline-block";
+    tally++;
+    console.log('number of tallies', tally);
 } else if (question3.toUpperCase() === 'N' || question1.toUpperCase() === 'NO') {
     alert('Incorrect');
 } else {
@@ -40,6 +48,8 @@ var question4 = prompt('Is Kris fond of coffee?');
 if (question4.toUpperCase() === 'N' || question1.toUpperCase() === 'NO') {
     alert('Correct!');
     document.getElementById("check4").style.display = "inline-block";
+    tally++;
+    console.log('number of tallies', tally);
 } else if (question4.toUpperCase() === 'Y' || question1.toUpperCase() === 'YES') {
     alert('Incorrect');
 } else {
@@ -52,6 +62,8 @@ var question5 = prompt('Does Kris have an endless stomach?');
 if (question5.toUpperCase() === 'Y' || question1.toUpperCase() === 'YES') {
     alert('Correct!');
     document.getElementById("check5").style.display = "inline-block";
+    tally++;
+    console.log('number of tallies', tally);
 } else if (question5.toUpperCase() === 'N' || question1.toUpperCase() === 'NO') {
     alert('Incorrect');
 } else {
@@ -78,6 +90,9 @@ while (wrongAnswer < 4) {
         //question6 = prompt('How many days a month does Kris tend abandon all self-control to spend to much money on ramen?');
     } else if (userGuess === rightAnswer) {
         alert('Exactly! (Maybe)');
+        document.getElementById("check6").style.display = "inline-block";
+        tally++;
+        console.log('number of tallies', tally);
         break;
     } else {
         alert('You never guessed a number!');
@@ -87,57 +102,44 @@ while (wrongAnswer < 4) {
 }  
     
 
-var question7 = prompt('Can you guess a country that Kris has visited?').toLowerCase();
-var canada = false;
-var italy = false;
-var netherlands = false;
-var countryList = ['canada', 'italy', 'the netherlands'];
+// var question7 = prompt('Can you guess a country that Kris has visited?').toLowerCase();
+// var canada = false;
+// var italy = false;
+// var netherlands = false;
+// var countryList = ['canada', 'italy', 'the netherlands'];
 
-//for (var attempts = 0; attempts < 6; attempts++) {
-   // console.log('attempt made', attempts);
-var attempts = 0;
-while (attempts <= 6 ) {
-    for (var i = 0; i < countryList.length; i++) {
-        if (question7 === countryList[i]) {
-            if (question7 === 'italy') {
-            alert('I have been been there!')
-            italy = true;
-            break;
-        } else if (question7 === 'canada') {
-            alert('I have been been there!')
-            canada = true;
-            break;
-        } else if (question7 === 'the netherlands') {
-            alert('I have been been there!')
-            netherlands = true;
-            break;
-        } 
-        }
-    }
-    if (italy === true || canada === true || netherlands === true) {
-        console.log('Question answered right', italy, canada, netherlands);
-        alert('I have been too Italy, The Netherlands, and Canada.');
-        break;
-    } else if (canada === false && italy === false && netherlands === false) {
-        attempts++;
-        console.log('attempt made', attempts);
-    }
+// var attempts = 0;
+// while (attempts <= 6 ) {
+//     for (var i = 0; i < countryList.length; i++) {
+//         if (question7 === countryList[i]) {
+//             if (question7 === 'italy') {
+//             alert('I have been been there!')
+//             italy = true;
+//             break;
+//         } else if (question7 === 'canada') {
+//             alert('I have been been there!')
+//             canada = true;
+//             break;
+//         } else if (question7 === 'the netherlands') {
+//             alert('I have been been there!')
+//             netherlands = true;
+//             break;
+//         } 
+//         }
+//     }
+//     if (italy === true || canada === true || netherlands === true) {
+//         console.log('Question answered right', italy, canada, netherlands);
+//         alert('I have been too Italy, The Netherlands, and Canada.');
+//         break;
+//     } else if (canada === false && italy === false && netherlands === false) {
+//         attempts++;
+//         console.log('attempt made', attempts);
+//     }
+// }
+
+
+if (tally <= 4) {
+    var finalCount = alert('Nice try ' + username + ' you got ' + tally + ' out of 7 questions correct.');
+} else if (tally > 4) {
+    var finalCount = alert('Congratulations ' + username + ' you got ' + tally + ' out of 7 questions correct!');
 }
-//if (attempts >= 6) {
-  //  console.log('attempts limit reached', attempts);
-    //alert('Too bad, your out of tries... I have been to Italy, The Netherlands, and Canada.')
-       
-//}
-//}
-
-
-
-
-
-
-
-// if (question7 !== countryList[i]) {
-//     console.log ('Incorrect attempts', attempts);
-//     alert('No, I haven\'t been there. (Yet!) Try again!');
-//     break;
-// } 
